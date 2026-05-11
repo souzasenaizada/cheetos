c21c380185681bd8cd81871eaa9ffb872e6f6c1b
souzasenaizada <erick.s.rodrigues11@aluno.senai.br>
2026-05-11 15:47:52 -0300

    atualização de brand

--- Salgadinho/admin.py
+++ Salgadinho/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from Salgadinho.models import Salgadinho
+from Salgadinho.models import Salgadinho, Brand
 
 # Register your models here.
 class SalgadinhoAdmin(admin.ModelAdmin):
     list_display = ('flavor', 'factory_day', 'value', 'taste', 'color')
     search_fields = ('flavor', 'factory_day', 'value', 'taste', 'color')
+    
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'brand')
+    search_fields = ('id', 'brand')
+
 
 admin.site.register(Salgadinho, SalgadinhoAdmin)    #PARAMÊTROS PARA A PAGINA DE ADMIN
+admin.site.register(Brand, BrandAdmin)
--- Salgadinho/models.py
+++ Salgadinho/models.py
@@ -8,6 +8,14 @@ class Salgadinho(models.Model):
     color = models.CharField(max_length=20, blank=True, null=True)
     factory_day = models.DateField()
     value = models.FloatField()
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
-        return self.flavor
+        return self.flavor 
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    brand = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.brand
